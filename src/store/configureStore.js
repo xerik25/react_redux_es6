5c5b8df695fd797dd9a042ef4da45011f5ab5466
Landonius Rex: King of the Landons <landon.cline@selectquotesenior.com>
2016-11-15 22:34:19 -0600

    Finished Chapter: Async in Redux

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,15 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 
 export default function configureStore(initialState) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const middleware = [thunk];
   return createStore(
     rootReducer,
     initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(applyMiddleware(...middleware))
   );
 }
