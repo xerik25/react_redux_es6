a3560090930065d4dd144b3256c370f95af3b97e
Landonius Rex: King of the Landons <landon.cline@selectquotesenior.com>
2017-01-14 14:19:26 -0600

    Chapter Async status and error handline: Finished Hide Preloader Based on status

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,28 @@
 // This comp handles the App template used on every page
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
+import {connect} from 'react-redux';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <Header/>
+        <Header loading={this.props.loading}/>
         {this.props.children}
       </div>
     );
   }
 }
 
+function mapStateToProps(state, ownProps) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  }
+}
+
 App.prototypes = {
-  children: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
-export default App;
+export default connect(mapStateToProps)(App);
---
+++ src/components/common/LoadingDots.js
@@ -0,0 +1,46 @@
+import React, {PropTypes} from 'react';
+
+/**
+ * @description
+ */
+class LoadingDots extends React.Component {
+  //TODO: what is the point for context here?
+  constructor(props, context) {
+    super(props, context);
+    this.state = {frame: 1};
+
+    this.interval = setInterval(() => {
+      this.setState({
+        frame: this.state.frame + 1
+      });
+    }, this.props.interval);
+  }
+
+  componentWillMount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    let dots = this.state.frame % (this.props.dots + 1);
+    let text = '';
+    while (dots > 0) {
+      text += '.';
+      dots--;
+    }
+    //TODO: What is the spread operator doing here?
+    return <span {...this.props}>{text}&nbsp;</span>;
+  }
+}
+
+//TODO: this is an interesting way to handle default props
+LoadingDots.defaultProps = {
+  interval: 300, dots: 3
+};
+
+LoadingDots.propTypes = {
+  interval: PropTypes.number,
+  dots: PropTypes.number
+};
+
+
+export default LoadingDots;
